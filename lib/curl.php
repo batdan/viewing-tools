b6688edf396deea5106260ed325c3ff85ced6474
Daniel Gomes <gomes.daniel.44@gmail.com>
2021-02-22 23:59:59 +0100

    [ADD] class curl

--- lib/curl.php
+++ lib/curl.php
@@ -1,121 +1,41 @@
 <?php
-namespace tools;
+namespace core;
 
 /**
- * Call Web services with Curl
- * PATCH | PUT | POST | GET
- *
- * @author Daniel Gomes
+ * Appels avec la méthode php cURL
+ * GET et POST
  */
 class curl
 {
-    /* generate body content from different data type encodings (JSON, form encoding, etc.) */
-    private static function encode(?array $postFields, bool $json, bool $formEncoded)
-    {
-        if ($json && $postFields) {
-            $postFields = json_encode($postFields);
-        } elseif ($formEncoded && $postFields) {
-            $postFields =  http_build_query($postFields);
-        }
-        return $postFields;
-    }
+    /**
+     * Conversion string en éléments d'url
+     *
+     * @param       string      $url            chaîne à transformer
+     * @param       array       $postfields     éléments à poster (tableau associatif)
+     * @param       array       $addCurlopt     permet de passer des options supplémentaire dans cURL - ex : un header
+     */
+    public static function curlPost($url, $postfields, $addCurlopt=null)
+	{
+        // Get cURL resource
+        $curl = curl_init();
 
-    /* PATCH request with cURL */
-    public static function curlPatch(
-        string $url,
-        ?array $postFields,
-        ?array $addCurlOptions=null,
-        bool $json=true,
-        bool $formEncoded=false
-    ) : array {
-        $curlOption = [
-            CURLOPT_RETURNTRANSFER  => 1,
-            CURLOPT_URL             => $url,
-            CURLOPT_USERAGENT       => 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
-            CURLOPT_CUSTOMREQUEST   => 'PATCH',
-            CURLOPT_POSTFIELDS      => self::encode($postFields, $json, $formEncoded),
-        ];
-        return self::curlAux($url, $addCurlOptions, $curlOption);
-    }
-
-    /* PUT request with cURL */
-    public static function curlPut(
-        string $url,
-        ?array $postFields,
-        ?array $addCurlOptions=null,
-        bool $json=true,
-        bool $formEncoded=false
-    ) : array {
-        $curlOption = [
-            CURLOPT_RETURNTRANSFER  => 1,
-            CURLOPT_URL             => $url,
-            CURLOPT_USERAGENT       => 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
-            CURLOPT_CUSTOMREQUEST   => 'PUT',
-            CURLOPT_POSTFIELDS      =>  self::encode($postFields, $json, $formEncoded),
-        ];
-        return self::curlAux($url, $addCurlOptions, $curlOption);
-    }
-
-    /* POST request with cURL */
-    public static function curlPost(
-        string $url,
-        ?array $postFields,
-        ?array $addCurlOptions=null,
-        bool $json=true,
-        bool $formEncoded=false
-    ) : array {
-        $curlOption = [
+        // Set some options - we are passing in a useragent too here
+        $curlOption = array(
             CURLOPT_RETURNTRANSFER  => 1,
             CURLOPT_URL             => $url,
             CURLOPT_USERAGENT       => 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
             CURLOPT_POST            => 1,
-            CURLOPT_POSTFIELDS      =>  self::encode($postFields, $json, $formEncoded),
-        ];
-        return self::curlAux($url, $addCurlOptions, $curlOption);
-    }
+            CURLOPT_POSTFIELDS      => $postfields,
+        );
 
-    /* GET request with cURL */
-    public static function curlGet(
-        string $url,
-        ?array $getFields=null,
-        ?array $addCurlOptions=null
-    ) : array {
-        // URL composition
-        if (!is_null($getFields) and sizeof($getFields) != 0) {
-            if (!strstr($url, '?')) {
-                $url .= '?';
-            } else {
-                $url .= '&';
-            }
-            $url .= http_build_query($getFields, null, '&', PHP_QUERY_RFC3986);
-        }
-
-        // default options
-        $curlOption = [
-            CURLOPT_RETURNTRANSFER  => 1,
-            CURLOPT_URL             => $url,
-            CURLOPT_USERAGENT       => 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
-        ];
-        return self::curlAux($url, $addCurlOptions, $curlOption);
-    }
-
-
-    /* Auxiliary method called by all Curl Methods */
-    private static function curlAux(string $url, ?array $addCurlOptions, ?array $curlOption): array
-    {
-        // Initialize Curl
-        $curl = curl_init();
-
-        // Insert supplementary cURL options
-        if (! is_null($addCurlOptions) && is_array($addCurlOptions)) {
-            foreach ($addCurlOptions as $k=> $v) {
+        // Insertion des options cURL supplémentaire
+        if (! is_null($addCurlopt) && is_array($addCurlopt)){
+            foreach ($addCurlopt as $k=>$v) {
                 $curlOption[$k] = $v;
             }
         }
-        $curlOption[CURLOPT_HEADER] = 1;
-        $curlOption[CURLOPT_VERBOSE] = 0;
 
-        // Handle HTTPS calls
+        // Gestion des appels en HTTPS
         if (stristr($url, 'https')) {
             $curlOption[CURLOPT_SSL_VERIFYHOST] = false;
             $curlOption[CURLOPT_SSL_VERIFYPEER] = false;
@@ -123,23 +43,77 @@ class curl
 
         curl_setopt_array($curl, $curlOption);
 
-        // send cURL request
+        // Send the request & save response to $resp
         $res = curl_exec($curl);
 
-        // Process output
-        $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-        $header = substr($res, 0, $headerSize);
-        $body = substr($res, $headerSize);
-        $httpReturnCode = null;
-        $httpReturnCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        // Close
+        // Close request to clear up some resources
         curl_close($curl);
 
-        return [
-            'httpCode'  => $httpReturnCode,
-            'res'       => $body,
-            'header'    => $header
-        ];
+        return $res;
+	}
+
+
+    /**
+     * Appel méthode cURL en GET
+     * passage des arguments get dans un array()
+     *
+     * @param       string      $url            chaîne à transformer
+     * @param       array       $getFields      arguments en get (tableau associatif)
+     * @param       array       $addCurlopt     permet de passer des options supplémentaire dans cURL - ex : un header
+     */
+    public static function curlGet2($url, $getFields=array(), $addCurlopt=null)
+    {
+        if (count($getFields) > 0) {
+            $get = array();
+            foreach ($getFields as $k=>$v) {
+                $get[] = $k . '=' . $v;
+            }
+            $url .= '?' . implode('&', $get);
+        }
+
+        return self::curlGet($url, $addCurlopt);
+    }
+
+
+    /**
+     * Conversion string en éléments d'url
+     *
+     * @param       string      $url            chaîne à transformer
+     * @param       array       $addCurlopt     permet de passer des options supplémentaire dans cURL - ex : un header
+     */
+    public static function curlGet($url, $addCurlopt=null)
+	{
+        // Get cURL resource
+        $curl = curl_init();
+
+        // Set some options - we are passing in a useragent too here
+        $curlOption = array(
+            CURLOPT_RETURNTRANSFER  => 1,
+            CURLOPT_URL             => $url,
+            CURLOPT_USERAGENT       => 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+        );
+
+        // Insertion des options cURL supplémentaire
+        if (! is_null($addCurlopt) && is_array($addCurlopt)){
+            foreach ($addCurlopt as $k=>$v) {
+                $curlOption[$k] = $v;
+            }
+        }
+
+        // Gestion des appels en HTTPS
+        if (stristr($url, 'https')) {
+            $curlOption[CURLOPT_SSL_VERIFYHOST] = false;
+            $curlOption[CURLOPT_SSL_VERIFYPEER] = false;
+        }
+
+        curl_setopt_array($curl, $curlOption);
+
+        // Send the request & save response to $resp
+        $res = curl_exec($curl);
+
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        return $res;
     }
 }
